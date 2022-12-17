4516919a3cae2f93f4da37c39ae2f53d4f57373e
jim <jimmyy5121@gmail.com>
2022-12-17 15:26:35 +0800

    add src

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+const port = 8780;
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world');
+});
+
+app.listen(port, () => {
+  console.log('now listening on port :', port);
+});
+
+function sayHello(person: string) {
+  return 'Hello, ' + person;
+}
+
+const user = 'jim';
+console.log(sayHello(user));
